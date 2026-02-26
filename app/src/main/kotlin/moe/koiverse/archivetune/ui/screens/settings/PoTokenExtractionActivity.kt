3510aa803b7bbc08b4741a43dd80eaf6e5742128
koiverse <itzmerinz.dc@gmail.com>
2026-02-26 14:12:34 +0700

    style(PoTokenExtractionActivity.kt): Add androidx.compose.ui.unit.dp missing import

--- app/src/main/kotlin/moe/koiverse/archivetune/ui/screens/settings/PoTokenExtractionActivity.kt
+++ app/src/main/kotlin/moe/koiverse/archivetune/ui/screens/settings/PoTokenExtractionActivity.kt
@@ -35,6 +35,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import moe.koiverse.archivetune.R
 import moe.koiverse.archivetune.innertube.utils.PoTokenGenerator
