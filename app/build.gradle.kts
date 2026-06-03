ffc4dd55807c02330f3550d4c29305652b4c60b9
sang765 <bemimnitran@gmail.com>
2026-06-03 10:08:02 +0700

    chore: update SDK dependency to v1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -324,7 +324,7 @@ dependencies {
     implementation(libs.accompanist.lyrics.core)
 
     if (discordSocialSdkAvailable) {
-        "gmsImplementation"("com.github.sang765:discord-social-sdk:v1.0.1")
+        "gmsImplementation"("com.github.sang765:discord-social-sdk:v1.0.2")
     }
 }
 
